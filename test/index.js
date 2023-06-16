d313c8c46cd8e663d8636760231f6a222728a2ca
Shakti <shaktirajbhandari@gmail.com>
2023-06-16 13:59:17 +0545

    day 2

---
+++ test/index.js
@@ -0,0 +1,18 @@
+// I am using var keyword
+var a="123";
+console.log(a); 
+
+//I am using let keyword
+let b='456';
+console.log(b);
+
+//I am using const keyword
+const c=789;
+console.log(c);
+
+// alert("HELLO WORLD!!!")
+// const age=prompt("How old are you?")
+// console.log(age)
+
+// const isBoss=confirm("Are you the boss?")
+// console.log(isBoss)
